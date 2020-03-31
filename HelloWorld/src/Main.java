38e1b0125cb4b3d81584e87d337d3b9170a1b122
BigMumf <cmumford@cmumford.com>
2020-03-31 15:11:33 -0700

    changed line 6 again

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Sorry World!");
     }
-    System.out.println("Sorry this has taken so long")
 }
